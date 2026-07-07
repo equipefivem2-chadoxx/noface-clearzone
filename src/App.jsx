db8ca72fbb5fa05bd5ebaeba684008a28c157fa6
Enzo <chadoxx75@gmail.com>
2026-07-07 17:36:31 +0200

    maj push

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import MapInterface from './pages/MapInterface';
+
+// Importation sécurisée avec l'extension .jsx pour les serveurs Linux
+import Home from './pages/Home.jsx';
+import MapInterface from './pages/MapInterface.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Page d'accueil avec la sélection des services */}
         <Route path="/" element={<Home />} />
-        {/* L'URL dynamique :faction permet d'avoir la même page pour LSPD, BCSO ou GLOBAL */}
+        
+        {/* L'URL dynamique :faction génère la carte pour LSPD, BCSO ou GLOBAL avec le même composant */}
         <Route path="/map/:faction" element={<MapInterface />} /> 
       </Routes>
     </BrowserRouter>
