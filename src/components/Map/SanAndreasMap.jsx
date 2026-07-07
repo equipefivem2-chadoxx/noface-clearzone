724cc70f5550f67b6fef1c0ccfadd54892169323
Enzo <chadoxx75@gmail.com>
2026-07-07 17:58:55 +0200

    maj push

--- src/components/Map/SanAndreasMap.jsx
+++ src/components/Map/SanAndreasMap.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, ImageOverlay } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+const gtaCrs = L.CRS.Simple;
+// On définit la zone de travail
+const bounds = [[0, 0], [8192, 8192]];
 
 const SanAndreasMap = () => {
   return (
-    <div className="absolute inset-0 z-0">
+    <div className="absolute inset-0 z-0 bg-[#020617]">
       <MapContainer 
-        center={[48.8566, 2.3522]} /* Coordonnées de Paris pour le test */
-        zoom={13} 
+        crs={gtaCrs} 
+        bounds={bounds} 
+        center={[4096, 4096]} 
+        zoom={-1} 
+        minZoom={-2} 
+        maxZoom={2} 
         zoomControl={false}
-        style={{ height: '100vh', width: '100vw' }}
+        style={{ height: '100%', width: '100%', backgroundColor: '#020617' }}
       >
-        <TileLayer
-          url="https://maps.plebmasters.de/gta5/realmap/M3/mapC_{z}_{x}.png"
+        {/* On charge l'image directement depuis ton propre site */}
+        <ImageOverlay
+          url="/map.jpg"
+          bounds={bounds}
         />
       </MapContainer>
     </div>
