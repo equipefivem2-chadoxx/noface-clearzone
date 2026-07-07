bd16e4ba07b00b4b7bd41358db7879caca861de8
Enzo <chadoxx75@gmail.com>
2026-07-07 17:51:25 +0200

    maj push

--- src/components/Map/SanAndreasMap.jsx
+++ src/components/Map/SanAndreasMap.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-
-// Configuration du monde plat de GTA V
-const gtaCrs = L.CRS.Simple;
-const bounds = [[-4000, -4000], [4000, 4000]];
 
 const SanAndreasMap = () => {
   return (
-    <div className="absolute inset-0 z-0 bg-[#020617]">
+    <div className="absolute inset-0 z-0">
       <MapContainer 
-        crs={gtaCrs} 
-        bounds={bounds} 
-        center={[0, 0]} /* POINT DE DÉPART DE LA CAMÉRA (Centre de la map) */
-        zoom={2}        /* NIVEAU DE ZOOM INITIAL */
-        minZoom={1} 
-        maxZoom={5} 
+        center={[48.8566, 2.3522]} /* Coordonnées de Paris pour le test */
+        zoom={13} 
         zoomControl={false}
-        style={{ height: '100%', width: '100%', backgroundColor: '#020617' }}
+        style={{ height: '100vh', width: '100vw' }}
       >
         <TileLayer
-          url="https://s.rsg.sc/sc/images/games/GTAV/map/render/radar/{z}/{x}/{y}.jpg"
-          noWrap={true}
-          bounds={bounds}
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </MapContainer>
     </div>
